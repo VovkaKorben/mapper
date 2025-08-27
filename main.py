a2a5437e1aa08145d438f50096ff905f7ff9ed91
VovkaKorben <vovakorben@gmail.com>
2025-08-27 12:28:21 +0300

    sudoku

--- main.py
+++ main.py
@@ -2,70 +2,6 @@ import pygame, json, copy, random, box
 from pygame.locals import *
 
 
-def load_nodes():
-    try:
-        with open("nodes.json") as n:
-            file_contents = n.read()
-            result = json.loads(file_contents)
-    except:
-        result = []
-    return result
-
-
-def save_nodes(nodes):
-    try:
-        nc = nodes.copy()
-        for n in nc:
-            n.pop("selected", None)
-            n.pop("used", None)
-        nc = json.dumps(nc, indent=4)
-        with open("nodes.json", "w") as n:
-            n.write(nc)
-        result = True
-    except:
-        result = False
-
-
-def RectFromNode(node, size):
-    return pygame.Rect(node["x"] - size, node["y"] - size, size * 2, size * 2)
-
-
-def NodeAtMouse(nodes, mouse_pos):
-    for n in nodes:
-        if RectFromNode(n, NODE_SIZE).collidepoint(mouse_pos):
-            return n
-    return None
-
-
-def NormalizeRect(rct):
-    x1, y1, x2, y2 = rct
-    if x1 > x2:
-        x1, x2 = x2, x1
-    if y1 > y2:
-        y1, y2 = y2, y1
-    return (x1, y1, x2 - x1, y2 - y1)
-
-
-def init_nodes(nodes):
-    for node in nodes:
-        node["selected"] = False
-        node["oldval"] = False
-    return nodes
-
-
-def AddNodes(nodes, cnt):
-    MARGIN = 30
-    for i in range(cnt):
-        node = {
-            "id": len(nodes),
-            "x": random.randint(MARGIN, SCREEN_WIDTH - MARGIN),
-            "y": random.randint(MARGIN, SCREEN_HEIGHT - MARGIN),
-        }
-        nodes.append(node)
-    return nodes
-
-
-mouse_down_pos, mouse_move_pos = (0, 0), (0, 0)
 CLR_BG = (234, 212, 252)
 CLR_BLACK = (0, 0, 0)
 CLR_RED = (255, 0, 0)
@@ -80,141 +16,135 @@ MODE_SELECTION = 3
 SCREEN_WIDTH = 640
 SCREEN_HEIGHT = 480
 
-mousemode = MODE_NONE
+MARGIN = 15
+GRID_SIZE = 9
+SQUARE_SIZE = 50
+FONT_SIZE = 30
+
+
+def create_grid(to_remove):
+
+    def unUsedInRow(grid, i, num):
+        return num not in grid[i]
+
+    def unUsedInCol(grid, j, num):
+        for i in range(9):
+            if grid[i][j] == num:
+                return False
+        return True
+
+    def checkIfSafe(grid, i, j, num):
+        return unUsedInRow(grid, i, num) and unUsedInCol(grid, j, num) and unUsedInBox(grid, i - i % 3, j - j % 3, num)
+
+    def unUsedInBox(grid, rowStart, colStart, num):
+        for i in range(3):
+            for j in range(3):
+                if grid[rowStart + i][colStart + j] == num:
+                    return False
+        return True
+
+    def fillBox(grid, row, col):
+        for i in range(3):
+            for j in range(3):
+                while True:
+
+                    # Generate a random number between 1 and 9
+                    num = random.randint(1, 9)
+                    if unUsedInBox(grid, row, col, num):
+                        break
+                grid[row + i][col + j] = num
+
+    def fillDiagonal(grid):
+        for i in range(0, 9, 3):
+
+            # Fill each 3x3 subgrid diagonally
+            fillBox(grid, i, i)
+
+    def fillRemaining(grid, i, j):
+        if i == 9:
+            return True
+        if j == 9:
+            return fillRemaining(grid, i + 1, 0)
+        if grid[i][j] != 0:
+            return fillRemaining(grid, i, j + 1)
+        for num in range(1, 10):
+            if checkIfSafe(grid, i, j, num):
+                grid[i][j] = num
+                if fillRemaining(grid, i, j + 1):
+                    return True
+                grid[i][j] = 0
+
+        return False
+
+    def removeKDigits(grid, k):
+        while k > 0:
+            cellId = random.randint(0, 80)
+            i = cellId // 9
+            j = cellId % 9
+            if grid[i][j] != 0:
+                grid[i][j] = 0
+                k -= 1
+
+    grid = [[0] * GRID_SIZE for _ in range(GRID_SIZE)]  # [[0] * GRID_SIZE] * GRID_SIZE
+    fillDiagonal(grid)
+    fillRemaining(grid, 0, 0)
+    removeKDigits(grid, to_remove)
+    return grid
 
 
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 clock = pygame.time.Clock()
-pygame.display.set_caption("nodes test")
+pygame.display.set_caption("Sudoku")
 pygame.font.init()
-my_font = pygame.font.SysFont("Courier New", FONT_SIZE)
-node_font = pygame.font.SysFont("Arial Black", 12)
+fnt = pygame.font.SysFont("Courier New Bold", FONT_SIZE)
+grid = create_grid(20)
+mx, my = -1, -1
+keys = {}
 
-nodes = load_nodes()
-nodes = init_nodes(nodes)
 running = True
 
 while running:
-    shift = (pygame.key.get_mods() & pygame.KMOD_SHIFT) 
     for event in pygame.event.get():
+        # print(event.type)
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_ESCAPE:
                 running = False
-            elif event.key == pygame.K_KP_PLUS:
-                nodes = AddNodes(nodes, 1)
-                nodes = init_nodes(nodes)  # reset node states
-            elif event.key == pygame.K_KP_MINUS:
-                nodes = nodes[:-1]
-                nodes = init_nodes(nodes)  # reset node states
-
+            elif (0 <= mx < GRID_SIZE) and (0 <= my < GRID_SIZE):
+                keystate = pygame.key.get_pressed()
+                for k in range(pygame.K_1, pygame.K_9 + 1):
+                    if keystate[k]:
+                        k -= pygame.K_0
+                        # print(mx, my)
+                        grid[my][mx] = -k
+                        break
         elif event.type == pygame.QUIT:
             running = False
 
         elif event.type == pygame.MOUSEMOTION:
-            mouse_move_pos = list(pygame.mouse.get_pos())
-            if mousemode == MODE_NODE:
-                delta = [mouse_move_pos[0] - mouse_down_pos[0], mouse_move_pos[1] - mouse_down_pos[1]]
-                # print(f"delta: {delta}")
-
-                for n in nodes:
-                    if n["selected"]:
-                        n["x"] += delta[0]
-                        n["y"] += delta[1]
-                mouse_down_pos = mouse_move_pos
-            elif mousemode == MODE_SELECTION:
-                rct = (
-                    mouse_down_pos[0],
-                    mouse_down_pos[1],
-                    mouse_move_pos[0],
-                    mouse_move_pos[1],
-                )
-                rct = pygame.Rect(NormalizeRect(rct))
-                for n in nodes:
-                    n["selected"] = n["oldval"] 
-                    if rct.collidepoint(n["x"], n["y"]):
-                        n["selected"] = not n["selected"]
+            mouse = list(pygame.mouse.get_pos())
+            mx, my = (mouse[0] - MARGIN) // SQUARE_SIZE, (mouse[1] - MARGIN) // SQUARE_SIZE
+            # print(x, y)
+            # if (0 <= mouse[0] < GRID_SIZE) and (0 <= mouse[1] < GRID_SIZE):
 
         elif event.type == pygame.MOUSEBUTTONDOWN:
-
-            mouse_down_pos = list(pygame.mouse.get_pos())
-            current_node = NodeAtMouse(nodes, mouse_down_pos)
-
-            if current_node == None:
-                # empty space clicked
-                if not shift:
-                    for n in nodes:
-                        n["selected"] = False
-                for n in nodes:
-                    n["oldval"] = n["selected"]
-
-                mousemode = MODE_SELECTION
-            else:
-                # node clicked
-                # if no SHIFT unselect all nodes
-                if not shift:  # or (shift and current_node["selected"] == False):
-                    for n in nodes:
-                        n["selected"] = False
-                current_node["selected"] = not current_node["selected"]
-                mousemode = MODE_NODE
-
-        elif event.type == pygame.MOUSEBUTTONUP:
-            mousemode = MODE_NONE
+            if (0 <= mx < GRID_SIZE) and (0 <= my < GRID_SIZE):
+                if grid[my][mx] < 0:
+                    grid[my][mx] = 0
+                # if keystate[pygame.K_LEFT]:            self.rot += self.rot_speed
     # bg
     screen.fill(CLR_BG)
 
-    # draw nodes
-    for n in nodes:
-        rct = RectFromNode(n, NODE_SIZE)
-        if n["selected"]:
-            pygame.draw.rect(screen, CLR_BLACK, rct)
-        else:
-            pygame.draw.rect(screen, CLR_YELLOW, rct)
-            pygame.draw.rect(screen, CLR_BLACK, rct, 2)
-        # node ID
-        text_surface = node_font.render(f"{n['id']}", True, CLR_BLACK)
-        r = text_surface.get_rect()
-        screen.blit(text_surface, (n["x"] + NODE_SIZE * 1.5, n["y"] - r[3] // 2))
+    for x in range(GRID_SIZE + 1):
+        thickness = 3 if (x % 3) == 0 else 1
+        pygame.draw.line(screen, CLR_BLACK, [MARGIN, MARGIN + x * SQUARE_SIZE], [MARGIN + GRID_SIZE * SQUARE_SIZE, MARGIN + x * SQUARE_SIZE], thickness)
+        pygame.draw.line(screen, CLR_BLACK, [MARGIN + x * SQUARE_SIZE, MARGIN], [MARGIN + x * SQUARE_SIZE, MARGIN + GRID_SIZE * SQUARE_SIZE], thickness)
 
-    # draw hover node info
-    line_pos = 5
-    node = NodeAtMouse(nodes, mouse_move_pos)
-    if node != None:
-        text_surface = my_font.render(f"NodeID: {node['id']}", True, CLR_BLACK)
-        ts = text_surface.get_rect()
-        screen.blit(text_surface, (SCREEN_WIDTH - ts.w - 5, line_pos))
-        line_pos += FONT_SIZE
-    if shift:
-        text_surface = my_font.render("SHIFT", True, CLR_BLACK)
-        ts = text_surface.get_rect()
-        screen.blit(text_surface, (SCREEN_WIDTH - ts.w - 5, line_pos))
-        line_pos += FONT_SIZE
-
-    # draw text info
-    line_pos = 0
-    if mousemode == MODE_NONE:
-        text_surface = my_font.render(f"x:{mouse_move_pos[0]} y:{mouse_move_pos[1]}", True, CLR_BLACK)
-        screen.blit(text_surface, (0, line_pos))
-        line_pos += FONT_SIZE
-    else:
-        text_surface = my_font.render(f"x:{mouse_down_pos[0]} y:{mouse_down_pos[1]}", True, CLR_BLUE)
-        screen.blit(text_surface, (0, line_pos))
-        line_pos += FONT_SIZE
-        text_surface = my_font.render(f"x:{mouse_move_pos[0]} y:{mouse_move_pos[1]}", True, CLR_BLACK)
-        screen.blit(text_surface, (0, line_pos))
-        line_pos += FONT_SIZE
-
-    # draw selection if exists
-    if mousemode == MODE_SELECTION:
-        rct = (
-            mouse_down_pos[0],
-            mouse_down_pos[1],
-            mouse_move_pos[0],
-            mouse_move_pos[1],
-        )
-        rct = NormalizeRect(rct)
-        pygame.draw.rect(screen, CLR_BLACK, rct, 1)
+    for y in range(GRID_SIZE):
+        for x in range(GRID_SIZE):
+            if grid[y][x] != 0:
+                text_surface = fnt.render(str(abs(grid[y][x])), True, CLR_BLACK if grid[y][x] > 0 else CLR_RED)
+                r = text_surface.get_rect()
+                screen.blit(text_surface, (MARGIN + x * SQUARE_SIZE + (SQUARE_SIZE - r[2]) // 2, MARGIN + y * SQUARE_SIZE + (SQUARE_SIZE - r[3]) // 2))
 
     pygame.display.flip()
-    clock.tick(60)
-
-save_nodes(nodes)
+    clock.tick(10)
